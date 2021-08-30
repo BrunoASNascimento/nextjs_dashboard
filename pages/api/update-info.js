047d8005e30c2fe6186870f9b614d09979f0c2b0
Bruno Nascimento <bruno-asn@outlook.com.br>
2021-08-29 22:49:22 -0300

    Update file

--- pages/api/update-info.js
+++ pages/api/update-info.js
@@ -1,8 +1,21 @@
-export default async function updateTable(request, response) {
-  if (request.method === "POST") {
-    console.log(request);
-    response.json({ message: "Teste" });
+import { connectToDatabase } from "../../util/mongodb";
+
+export default async function updateTable(req, res) {
+  if (req.method === "POST") {
+    console.log(req.body);
+    const { db } = await connectToDatabase();
+    const ObjectID = require("mongodb").ObjectId;
+
+    const infoTableUpdated = await db
+      .collection("next_front_table")
+      .updateOne(
+        { _id: ObjectID(req.body.id) },
+        { $set: { allocation: req.body.allocation } }
+      );
+
+    res.json(infoTableUpdated);
+
     return;
   }
-  response.status(404).json({ message: "Ainda não temos nada no GET." });
+  res.status(404).json({ message: "Ainda não temos nada no GET." });
 }
